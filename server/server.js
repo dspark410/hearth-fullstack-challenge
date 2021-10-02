9c61a6c255b5aec81379acfbfe902c0bc7858217
David Park <dspark410@gmail.com>
2021-10-01 17:07:06 -0700

    prepares for deployment

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.get('/api/redfin/sanfrancisco', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/build'))
 
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'))
   })
 }
